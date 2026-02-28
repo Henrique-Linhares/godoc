e6a66c1056135fc63f751974d31f84d074a393b6
HENRIQUE LINHARES PINHEIRO LOIOLA <henrique.loiola@senaisp.edu.br>
2026-02-27 21:40:02 -0300

    Iniciando o projeto

---
+++ godoc/src/main/java/com/spring/godoc/entity/enums/StatusAgendamento.java
@@ -0,0 +1,7 @@
+package com.spring.godoc.entity.enums;
+
+public enum StatusAgendamento {
+    AGENDADO,
+    CONCLUIDO,
+    CANCELADO
+}
---
+++ godoc/src/test/java/com/spring/godoc/GodocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.godoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
