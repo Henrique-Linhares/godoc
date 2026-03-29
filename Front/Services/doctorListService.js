172d6b5f3ef490b929ab5501b18a1356c105e6c9
VictorLemosbar <victor.escolas9045@gmail.com>
2026-03-29 19:03:22 -0300

    DoxCard melhorado com estrutura definida, exceto imagens e rendierização condicional

--- Front/Services/doctorListService.js
+++ Front/Services/doctorListService.js
@@ -1,4 +1,4 @@
-export  const doctors = [
+export const doctors = [
   {
     id: 1,
     name: "Dr. Cleiton Rasta",
--- Front/app/components/Button/HomeComponents/DocCard/DocCard.tsx
+++ Front/app/components/Button/HomeComponents/DocCard/DocCard.tsx
@@ -5,8 +5,30 @@ import { doctors } from '@/Services/doctorListService'
 
 function DoxCard() {
     return (
-        <div className={styles.docBoxContainer}>
-
+        <div className={styles.container}>
+            <div className={styles.card}>
+                {doctors.map((item, index) => {
+                    return (
+                        <div className={styles.info}>
+                            <div className={styles.details}>
+                                <span>{item.name}</span>
+                                <div className={styles.meta}>
+                                    <span>{item.specialty}</span>
+                                    <span>{item.location}</span>
+                                </div>
+                                <div className={styles.ratings}>
+                                    <span>{item.rating}</span>
+                                    <span>{item.reviews}</span>
+                                </div>
+                            </div>
+                            <div className={styles.scheduleInfo}>
+                                <span>{item.availableToday}</span>
+                                <span>{item.action.label}</span>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
