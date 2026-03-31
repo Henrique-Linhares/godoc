2e3d4e50d87f15e56f4c0b82575d49d9d9e13ca7
HENRIQUE LINHARES PINHEIRO LOIOLA <henrique.loiola@senaisp.edu.br>
2026-03-30 21:35:00 -0300

    Adicionando o Tratamento de Exceções

--- Back-End/godoc/src/main/java/com/spring/godoc/domains/agendamento/AgendamentoService.java
+++ Back-End/godoc/src/main/java/com/spring/godoc/domains/agendamento/AgendamentoService.java
@@ -1,7 +1,30 @@
 package com.spring.godoc.domains.agendamento;
 
+import com.spring.godoc.domains.agendamento.dtos.responses.AgendamentoResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AgendamentoService {
+
+    @Autowired
+    private AgendamentoRepository agendamentoRepository;
+
+    public AgendamentoService(AgendamentoRepository agendamentoRepository) {
+        this.agendamentoRepository = agendamentoRepository;
+    }
+
+    // Buscar por ID
+    public AgendamentoResponse buscarPorId(Long id) {
+        AgendamentoEntity agendamento = agendamentoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Agendamento não encontrado com ID: " + id));
+
+        return new AgendamentoResponse(
+                agendamento.getId(),
+                agendamento.getData(),
+                agendamento.getStatus(),
+                agendamento.getPaciente(),
+                agendamento.getMedico()
+        );
+    }
 }
