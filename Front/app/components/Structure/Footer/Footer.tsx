575b8d884fcc0108c6fa1d12652490297b9b4c73
VictorLemosbar <victor.escolas9045@gmail.com>
2026-05-24 14:45:00 -0300

    Incrementado Footer

--- Front/app/components/Structure/Footer/Footer.tsx
+++ Front/app/components/Structure/Footer/Footer.tsx
@@ -2,30 +2,49 @@ import styles from "./footer.module.css"
 import Image from "next/image"
 
 function Footer() {
-    return (
-        <div className={styles.footerContainer}>
-            <div className={styles.itemBox}>
-                <div className={styles.logo}>
-                    <Image src={'/godoc_logo.png'} alt="Godoc" width={250} height={250} />
-                </div>
-                <div className={styles.contacContainer}>
-                    HELLO
-                </div>
-                <div className={styles.suportContainer}>
-                    <span>hlinhares453@gmail.com</span>
-                    <span>guihhsoaress@gmail.com</span>
-                    <span>(16) 99353-6708</span>
-                </div>
-                <div className={styles.devContainer}>
-                    <span className={styles.item}>Henrique Linhares </span>
-                    <span className={styles.item}>Guilherme Soares</span>
-                    <span className={styles.item}>Kaique Santos</span>
-                    <span className={styles.item}>Tatiana Silva</span>
-                    <span className={styles.item}>Victor Lemos</span>
-                </div>
-            </div>
+  return (
+    <div className={styles.footerContainer}>
+      <div className={styles.itemBox}>
+        <div className={styles.logo}>
+          <Image src={'/godoc_logo.png'} alt="Godoc" width={250} height={250} />
         </div>
-    )
+        <div className={styles.itemBox}>
+          <div className={styles.contacContainer}>
+            <strong>Sobre</strong>
+            <div className={styles.box}>
+              <label>Somos uma plataforma de agendamento de consultas médicas, criada para simplificar o acesso à saúde. Conectamos pacientes e profissionais de forma rápida, segura e sem complicações.</label>
+            </div>
+          </div>
+          <div className={styles.suportContainer}>
+            <strong>Contato</strong>
+            <div className={styles.box}>
+              <label>hlinhares453@gmail.com</label>
+              <label>guihhsoaress@gmail.com</label>
+              <label>(16) 99353-6708</label>
+              <Image
+                src="/instagram.png"
+                alt="Instagram"
+                width={20}
+                height={20}
+              />
+            </div>
+          </div>
+          <div className={styles.devContainer}>
+            <strong>Desenvolvedores</strong>
+            <div className={styles.box}>
+              {/* {devNames.map((item) => (
+              <label>{item}</label>
+            ))} */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  );
 }
+
+
+
 
 export default Footer
