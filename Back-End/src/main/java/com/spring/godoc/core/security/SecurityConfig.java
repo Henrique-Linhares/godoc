231f689fcaa3e29709d7ea7ad2a4cd51ba1e9f79
KAIQUE DOS SANTOS DA SILVA <kaique.silva118@senaisp.edu.br>
2026-05-26 19:44:01 -0300

    hotfix: Adicionado a porta 3000 no cors

--- Back-End/src/main/java/com/spring/godoc/core/security/SecurityConfig.java
+++ Back-End/src/main/java/com/spring/godoc/core/security/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
         httpSecurity
             .cors(cors -> cors.configurationSource(request -> {
                 CorsConfiguration config = new CorsConfiguration();
-                config.setAllowedOrigins(Arrays.asList("http://localhost:8080"));
+                config.setAllowedOrigins(Arrays.asList("http://localhost:8080", "http://localhost:3000"));
                 config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                 config.setAllowedHeaders(Arrays.asList("*"));
                 config.setAllowCredentials(true);
