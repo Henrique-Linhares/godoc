95625339f667cb588d3784a0f3b2f942e2bec6d0
VictorLemosbar <victor.escolas9045@gmail.com>
2026-06-01 21:00:10 -0300

    Criado requsição cadastro

--- Front/Services/doctorListService.js
+++ Front/Services/doctorListService.js
@@ -53,7 +53,18 @@ export async function cosultarPacientes() {
 
 }
 
+export async function cadastrar(email, password) {
+  const response = await fetch('http://localhost:8080/auth/register',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(dados)
+    }
+  )
+  const data = await response.json()
 
-
-
+  return data
+}
 
