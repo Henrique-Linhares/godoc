fc6362d0e5d0f0d2a5a6da129c8c110d8b2d3fb4
KAIQUE DOS SANTOS DA SILVA <kaique.silva118@senaisp.edu.br>
2026-06-02 21:09:10 -0300

    FEAT: Criação do enpoint getAllAgendamentos e ajustes no swegger

--- Back-End/src/main/java/com/spring/godoc/modules/agendamento/AgendamentoController.java
+++ Back-End/src/main/java/com/spring/godoc/modules/agendamento/AgendamentoController.java
@@ -4,10 +4,9 @@ import com.spring.godoc.modules.agendamento.dtos.requests.AgendamentoRequestDTO;
 import com.spring.godoc.modules.agendamento.dtos.responses.AgendamentoResponseDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/agendamentos")
@@ -23,5 +22,10 @@ public class AgendamentoController {
     public ResponseEntity<AgendamentoResponseDTO> criar(@RequestBody AgendamentoRequestDTO dto) {
         AgendamentoResponseDTO response = agendamentoService.criar(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AgendamentoResponseDTO>> getAllAgendamentos() {
+        return ResponseEntity.ok(agendamentoService.getAllAgendamentos());
     }
 }
